83c75a228fa147f9e9ca691f1bdf08f8bd18b0f4
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2016-06-18 19:03:48 -0400

    Metodo funcional para obtener cuentas desde BEstado

--- acciones/external/bancoestado.php
+++ acciones/external/bancoestado.php
@@ -1,41 +1,120 @@
 <?php defined("INCLUDED") or die("nel");
-// https://github.com/jkcgs/BankAccountMgr/blob/master/js/banks_modules/estado.js
 
-$ubase = "https://bancapersonas.bancoestado.cl/eBankingBech/";
-$formURL = $ubase . "login/login.htm";
-$postURL = $ubase . "login";
-$post2URL = "https://personas.bancoestado.cl/bancoestado/process.asp?MID=&AID=LOGIN-001";
-$resumenURL = $ubase . "superCartola/superCartola.htm";
-$homeURL = $ubase . "home/home.htm";
-$logoutURL = "seguridad/logoutIN.htm";
+// Imports
+require '../vendor/autoload.php';
+use \Curl\Curl;
+use PHPHtmlParser\Dom;
 
-$ch = curl_init();
-$d = wget($formURL, $ch);
+// Pre-definiciones
 
-$token = find_text($d, 'ctoken" value="', '"');
-$login_data = array(
-    'j_username' => $config['be_user'],
-    'j_password' => $config['be_pass'],
-    'ctoken' => $token
-);
-$headers = array('User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36');
-$login_res = wpost($postURL, $login_data, $ch, $headers);
+// Se debe retornar json
+header('Content-Type: text-json');
 
-if(strpos($login_res, "Home - BUILD") === false) {
-    echo "no login xd\n<pre style='white-space: normal'>";
-    echo str_replace("<", "&lt;", $login_res);
-    echo "</pre>";
+// User-Agent (Chrome 51)
+$UA = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
 
-    echo "<pre style='white-space: normal'>";
-    echo str_replace("<", "&lt;", wget($resumenURL, $ch));
-    echo "</pre>";
+// Inicializar Curl
+$curl = new Curl();
+$curl->setUserAgent($UA);
+$curl->setCookieFile(NULFILE);
+$curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+$curl->setOpt(CURLOPT_RETURNTRANSFER, true);
+$curl->setOpt(CURLOPT_FOLLOWLOCATION, true);
 
-    wget($logoutURL, $ch);
-    curl_close($ch);
-    die();
+// Funciones
+
+function clear_html($s) {
+    return str_replace("<", "&lt;", $s);
 }
 
-echo "todo bien todo correcto y yo que xd";
+function check_error($c) {
+    // revisar error
+    if ($c->error) {
+        echo json_encode([
+            'error' => $c->errorMessage,
+            'code' => $c->errorCode,
+            'curl_error' => true
+        ]);
+        exit;
+    }
+}
 
-wget($logoutURL, $ch);
-curl_close($ch);
+function throw_wrong() {
+    echo json_encode([
+        'error' => 'Wrong page',
+        'code' => -1
+    ]);
+    exit;
+}
+
+////////////////////////////////////////////////////////////
+// Inicio proceso
+
+// Pre-definiciones
+$prefix = "https://bancapersonas.bancoestado.cl/eBankingBech/";
+$loginPage = $prefix."login/login.htm";
+$loginPost = $prefix."login";
+$logoutPage = $prefix."seguridad/logoutIN.htm";
+$resumePage = $prefix."superCartola/superCartola.htm";
+
+//////// Paso 1: cargar login
+
+$curl->get($loginPage);
+
+// revisar error
+check_error($curl);
+
+// revisar página correcta
+if(strpos($curl->response, "BancoEstado Login") === false) {
+    throw_wrong();
+}
+
+$ctoken = find_text($curl->response, 'ctoken" value="', '"');
+
+//////// Paso 2: Login
+
+$curl->post($loginPost, array(
+    'j_username' => $config['be_user'],
+    'j_password' => $config['be_pass'],
+    'ctoken' => $ctoken
+));
+
+// revisar error
+check_error($curl);
+if(strpos($curl->response, "Home - BUILD") === false) {
+    throw_wrong();
+}
+
+//////// Paso 3: Recuperar datos
+
+$curl->get($resumePage);
+
+// revisar error
+check_error($curl);
+if(strpos($curl->response, "Resumen de Productos") === false) {
+    throw_wrong();
+}
+
+$dom = new Dom;
+$dom->load($curl->response);
+
+$domcuentas = $dom->getElementById('table_1')->find('tbody tr');
+$cuentas = [];
+
+foreach($domcuentas as $cuenta) {
+    $conts = $cuenta->find('td');
+    $nc = [
+        'nombre' => trim($conts[0]->find('span')[0]->text),
+        'numero' => trim($conts[1]->text),
+        'moneda' => trim($conts[2]->text),
+        'dinero' => preg_replace('/[^0-9]/', '', trim($conts[3]->text))
+    ];
+
+    $cuentas[] = $nc;
+}
+
+echo json_encode($cuentas);
+
+//////// Paso 4: Logout
+
+$curl->get($logoutPage);
