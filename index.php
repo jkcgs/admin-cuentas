3a950d62fa4a92f7a80810e55face62fa76211ae
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2016-07-04 15:08:12 -0400

    No iniciar si no está el archivo de config

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 define("INCLUDED", 1);
 define("VERSION", "0.0.1-alpha");
 
+if(!file_exists("app/config/config.php")) {
+	die("El archivo de configuración no existe. Puede utilizar el archivo config.example.php para crearlo.");
+}
+
 require 'vendor/autoload.php';
 require 'app/init.php';
