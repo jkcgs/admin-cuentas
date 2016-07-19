b11d48783a9fb1611558241a86fda63e4af237b7
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2016-07-18 22:14:09 -0400

    Quitar código en desuso

--- app/api/debts/get_merged.php
+++ app/api/debts/get_merged.php
@@ -1,27 +1,22 @@
 <?php defined("INCLUDED") or die("Acceso denegado."); try_logged();
 require "app/includes/database.php";
 
-if(!isset($_GET['id'])) {
-    $q1 = $db->query("SELECT * FROM deudores order by nombre asc");
-    if(!$q1) {
-        throw_error($db->error);
-    }
-
-    $q2 = $db->query(
-        "SELECT d.id, d.deudor, de.nombre AS nombreDeudor, d.descripcion, d.monto, d.fecha, d.pagada ".
-        "FROM deudas AS d INNER JOIN deudores AS de ON d.deudor = de.id ".
-        "ORDER BY id DESC, fecha DESC"
-    );
-    if(!$q2) {
-        throw_error($db->error);
-    }
-    
-    echo json_encode([
-        "success" => true,
-        "debtors" => dbGetAll($q1),
-        "debts" => dbGetAll($q2)
-    ]);
-
-} else {
-    echo get_id('cuentas');
+$q1 = $db->query("SELECT * FROM deudores order by nombre asc");
+if(!$q1) {
+    throw_error($db->error);
 }
+
+$q2 = $db->query(
+    "SELECT d.id, d.deudor, de.nombre AS nombreDeudor, d.descripcion, d.monto, d.fecha, d.pagada ".
+    "FROM deudas AS d INNER JOIN deudores AS de ON d.deudor = de.id ".
+    "ORDER BY id DESC, fecha DESC"
+);
+if(!$q2) {
+    throw_error($db->error);
+}
+
+echo json_encode([
+    "success" => true,
+    "debtors" => dbGetAll($q1),
+    "debts" => dbGetAll($q2)
+]);
