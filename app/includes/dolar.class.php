8bfb3fd5ee47d61296e498e8e6b9c4fc020c961e
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2016-09-11 01:31:47 -0300

    Fix #9 más tweaks visuales

--- app/includes/dolar.class.php
+++ app/includes/dolar.class.php
@@ -1,10 +1,10 @@
 
 <?php 
+
 /**
 * Código para obtener el precio del dolar desde la Bolsa Electrónica de Santiago
 * Basado en: https://gist.github.com/isseu/8099933
 */
-
 class DolarAPI
 {
 	private $dominio = "http://www.bolchile.cl";
@@ -18,13 +18,27 @@ class DolarAPI
       		$arrResult[] = $data;
     	}
 
-    	return (float)(str_replace(',','.',end($arrResult)[1]));
+    	$val = (float)(str_replace(',','.',end($arrResult)[1]));
+		return $val == 0 ? $this->getFromSite() : $val;
 	}
 
 	private function getData()
 	{
 		return fopen($this->dominio . $this->url, "r");
 	}
-}
 
-?>
+	private function getFromSite() {
+		$site = file_get_contents("http://www.bolchile.cl/Dolar?menu=DOLAR");
+		if(empty(trim($site))) {
+			return 0;
+		}
+
+		// Encontrar el texto con el valor
+		$init = strpos($site, 'id="ultimoPrecio"');
+		$d_init = strpos($site, ">", $init) + 1;
+		$d_end = strpos($site, "<", $d_init) - $d_init;
+
+		$val = substr($site, $d_init, $d_end);
+		return floatval(str_replace(",", ".", $val));
+	}
+}
